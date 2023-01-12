3b7f9968e0cdb07e14fa6a663988554f722a0214
DownGrowth <1209157041@qq.com>
2023-01-12 18:54:30 +0800

    完成第一个页面

--- src/pages/Welcome1.tsx
+++ src/pages/Welcome1.tsx
@@ -1,8 +1,8 @@
 import p1 from '../assets/images/welcome1.svg'
 export const Welcome1: React.FC = () => {
-  return (<div>
+  return (<div >
     <img src={p1}/>
-    <h2>
+    <h2 text-center>
       高山流水 <br />
       帮您省钱
     </h2>
