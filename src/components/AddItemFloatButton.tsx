5bbc249effd814566343e409f8c809f13a3b07cf
DownGrowth <1209157041@qq.com>
2023-01-17 18:21:37 +0800

    引入svgsprites

--- src/components/AddItemFloatButton.tsx
+++ src/components/AddItemFloatButton.tsx
@@ -1,5 +1,6 @@
-import add from '../assets/images/add.svg'
 export const AddItemFloatButton: React.FC = () =>
-  (<button w-56px h-56px bg='#C1E9F8' rounded="50%" b-none text='black' text-6xl fixed bottom-16px right-16px>
-  <img max-w="70%" max-h="70%" src={add} />
+  (<button w-56px h-56px bg='#C1E9F8' rounded="50%" b-none text='black' fixed bottom-16px right-16px>
+  <svg style={{ fill: '#666666', width: '2.4em', height: '2.4em' }}>
+    <use xlinkHref='#add'></use>
+  </svg>
 </button>)
