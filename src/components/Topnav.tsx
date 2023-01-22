83fe003726c71e68b70caff604f70d929e0ce202
DownGrowth <1209157041@qq.com>
2023-01-22 15:54:27 +0800

    完成时间范围选择组件 TimeRangePicker

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 export const TopNav: React.FC<Props> = ({ title = '山音记账' }) => {
   return (
-    <div text="#6a5e73" flex items-center p-16px>
+    <div text="#6a5e73" flex items-center pt-24px pb-8px px-24px>
       <Icon name='menu' className='w-24px h-24px mr-16px' />
       <h1 text-24px>{title}</h1>
     </div>
--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 import { AddItemFloatButton } from '../components/AddItemFloatButton'
+import type { TimeRange } from '../components/TimeRangePicker'
 import { TimeRangePicker } from '../components/TimeRangePicker'
 import { TopNav } from '../components/Topnav'
 import { ItemList } from './ItemsList'
@@ -10,11 +12,12 @@ background:linear-gradient(0deg, rgba(193,233,248,1) 0%, rgba(255,252,237,1) 100
 `
 
 export const ItemsPage: React.FC = () => {
+  const [timeRange, setTimeRange] = useState <TimeRange>('thisMonth')
   return (
     <div>
       <Div>
         <TopNav />
-        <TimeRangePicker />
+        <TimeRangePicker selected={timeRange} onSelected={setTimeRange} />
       </Div>
       <ItemSummary />
       <ItemList />
