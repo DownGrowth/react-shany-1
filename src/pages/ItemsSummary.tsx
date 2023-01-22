0871293f828bda4dbf60bcf66bc7d3f0ffe71a1a
DownGrowth <1209157041@qq.com>
2023-01-22 16:28:42 +0800

    完成ItemSummary组件

--- src/pages/ItemsSummary.tsx
+++ src/pages/ItemsSummary.tsx
@@ -1,3 +1,18 @@
 export const ItemSummary: React.FC = () => {
-  return <div>ItemSummary </div>
+  return (
+    <ol bg="#f1f7e6" flex justify-between items-center m-16px rounded-24px py-12px children-px-24px text-center>
+      <li text='#d47678'>
+        <div>收入</div>
+        <div>1000</div>
+      </li>
+      <li text="#5099ac">
+        <div>支出</div>
+        <div>1000</div>
+      </li>
+      <li text='#666666'>
+        <div>净收入</div>
+        <div>1000</div>
+      </li>
+    </ol>
+  )
 }
