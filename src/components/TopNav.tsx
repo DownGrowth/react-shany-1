c773482d2f42fe97994ced191f7c467ed5c29542
DownGrowth <1209157041@qq.com>
2023-01-23 21:14:33 +0800

    跳转路由

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -8,7 +8,8 @@ export const TopNav: React.FC<Props> = ({ title = '山音记账' }) => {
   const { visible, setVisible } = useMenuStore()
   return (
   <div text="[var(--text-main)]" flex items-center pt-24px pb-8px px-24px>
-      <Icon name='menu' className='w-24px h-24px mr-16px' onClick={() => { setVisible(!visible) }}/>
+      <Icon name='menu' className='w-24px h-24px mr-16px'
+        onClick={() => { setVisible(!visible) }} />
       <h1 text-24px>{title}</h1>
     </div>
   )
