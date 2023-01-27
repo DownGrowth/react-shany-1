10fa672decb8fa8afcdd4f8b4a0f231eb3c65f2c
DownGrowth <1209157041@qq.com>
2023-01-27 23:15:58 +0800

    完成顶部导航栏

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
+import { Tabs } from '../components/Tabs'
 import { TopNav } from '../components/TopNav'
 
+type itemKind = 'income' | 'expenses'
 export const ItemsNewPage: React.FC = () => {
+  const tabItems: { key: itemKind; text: string }[]
+    = [{ key: 'income', text: '收入' }, { key: 'expenses', text: '支出' }]
+  const [tabItem, setTabItem] = useState<itemKind>('expenses')
   return (
     <div>
       <Gradient>
         <TopNav title='记一笔' icon={<Icon name="back" className='w-24px h-24px' />} />
+        <Tabs tabItems={tabItems} value={tabItem} onChange={item => setTabItem(item) } />
       </Gradient>
     </div>
   )
