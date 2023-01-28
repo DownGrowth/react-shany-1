e6cec37a54dc0f2f4bde03aadb71140417b39fce
DownGrowth <1209157041@qq.com>
2023-01-28 16:32:20 +0800

    Tabs 支持自定义三处的样式

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import vhCheck from 'vh-check'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
 import './global.scss'
 import 'virtual:uno.css'
 import './app.scss'
 import 'virtual:svgsprites'
+vhCheck()
 const div = document.getElementById('root') as HTMLElement
 
 const root = ReactDOM.createRoot(div)
