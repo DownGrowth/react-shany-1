e5a680f371bd09f4ebb459a258af74cbc881ccf6
DownGrowth <1209157041@qq.com>
2023-01-28 20:44:11 +0800

    更改

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -17,7 +17,7 @@ export const ItemsNewPage: React.FC = () => {
       <Gradient className="grow-0 shrink-0">
         <TopNav title='记一笔' icon={<Icon name="back" className='w-24px h-24px' />} />
         </Gradient>
-      <Tabs tabItems={tabItems} className="text-center grow-1 shrink-1 overflow-hidden" classPrefix='ItemsNewPage' value={tabItem} onChange={tabItem => setTabItem(tabItem)} />
+      <Tabs tabItems={tabItems} className="text-center grow-1 shrink-1 overflow-hidden" classPrefix='itemsNewPage' value={tabItem} onChange={tabItem => setTabItem(tabItem)} />
       <DateAndAmount className="grow-0 shrink-0"/>
     </div>
   )
