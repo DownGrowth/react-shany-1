5dfba70a2922a33e9f404db4a559e10cf16d6bfb
DownGrowth <1209157041@qq.com>
2023-01-30 22:41:52 +0800

    完成对DatePciker的封装

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,70 +1,64 @@
 import { useState } from 'react'
-import { time, Time } from '../lib/time'
+import { time } from '../lib/time'
 
 type Props = {
   start?: Date
   end?: Date
   value?: Date
+  itemHeight?: number
 }
 export const DatePicker: React.FC<Props> = (props) => {
-  const { start, end, value } = props
-  const t = time()
-  console.log(t.format('yyyy年MM月dd日 HH:mm:ss.fff'))
+  const { start, end, value, itemHeight = 36 } = props
+  const startTime = start ? time(start) : time().add(-10, 'year')
+  const endTime = end ? time(end) : time().add(10, 'year')
+  const valueTime = value ? time(value) : time()
+  if (endTime.timestamp <= startTime.timestamp) {
+    throw new Error('结束时间必须晚于开始时间')
+  }
+  const yearList = Array.from({ length: endTime.year - startTime.year + 1 }).map((_, index) => startTime.year + index)
+  const index = yearList.indexOf(valueTime.year)
   const [isTouching, setIsTouching] = useState(false)
   const [lastY, setLastY] = useState(-1)
-  const [translateY, setTranslateY] = useState(0)
+  const [translateY, _setTranslateY] = useState(index * -itemHeight)
+  const setTranslateY = (y: number) => {
+    y = Math.min(y, 0)
+    y = Math.max(y, (yearList.length - 1) * -itemHeight)
+    _setTranslateY(y)
+  }
   return (
     <div h="50vh" overflow-hidden relative
-    onTouchStart={(e) => {
-      setIsTouching(true)
-      setLastY(e.touches[0].clientY)
-    }}
-    onTouchMove={(e) => {
-      if (isTouching) {
-        const y = e.touches[0].clientY
-        const dy = y - lastY
-        setTranslateY(translateY + dy)
-        setLastY(y)
-      }
-    }}
-    onTouchEnd={() => {
-      const reaminder = translateY % 36
-      let y = translateY - reaminder
-      if (Math.abs(reaminder) >= 18) {
-        y += 36 * (reaminder > 0 ? 1 : -1)
-      }
-      setTranslateY(y)
-      setIsTouching(false)
-    }}
-  >
-    <div b-1 b-red h-36px absolute top="[calc(50%-18px)]" w-full />
-    <div absolute top="[calc(50%-18px-108px)]" w-full>
-      <ol style={{ transform: `translateY(${translateY}px)` }}
-        children-h-36px text-center children-leading-36px>
-        <li>2000</li>
-        <li>2001</li>
-        <li>2002</li>
-        <li>2003</li>
-        <li>2004</li>
-        <li>2005</li>
-        <li>2006</li>
-        <li>2007</li>
-        <li>2008</li>
-        <li>2009</li>
-        <li>2010</li>
-        <li>2000</li>
-        <li>2001</li>
-        <li>2002</li>
-        <li>2003</li>
-        <li>2004</li>
-        <li>2005</li>
-        <li>2006</li>
-        <li>2007</li>
-        <li>2008</li>
-        <li>2009</li>
-        <li>2010</li>
-      </ol>
+      onTouchStart={(e) => {
+        setIsTouching(true)
+        setLastY(e.touches[0].clientY)
+      }}
+      onTouchMove={(e) => {
+        if (isTouching) {
+          const y = e.touches[0].clientY
+          const dy = y - lastY
+          setTranslateY(translateY + dy)
+          setLastY(y)
+          console.log(translateY)
+        }
+      }}
+      onTouchEnd={() => {
+        const reaminder = translateY % itemHeight
+        let y = translateY - reaminder
+        if (Math.abs(reaminder) >= itemHeight / 2) {
+          y += itemHeight * (reaminder > 0 ? 1 : -1)
+        }
+        setTranslateY(y)
+        setIsTouching(false)
+      }}
+    >
+      <div b-1 b-red absolute top="50%" w-full
+        style={{ height: itemHeight, transform: `translateY(${-itemHeight / 2}px)` }} />
+      <div absolute top="50%" w-full style={{ transform: `translateY(${-itemHeight / 2}px)` }}>
+        <ol style={{ transform: `translateY(${translateY}px)` }} text-center children-flex children-items-center children-justify-center>
+          {yearList.map(year =>
+            <li key={year} style={{ height: itemHeight }}>{year}</li>
+          )}
+        </ol>
+      </div>
     </div>
-  </div>
   )
 }
