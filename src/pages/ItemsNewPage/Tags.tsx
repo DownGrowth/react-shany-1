9f4607d6c98c52ff259fa0df73539178b96da6dc
DownGrowth <1209157041@qq.com>
2023-01-31 20:03:22 +0800

    创建新建标签页面

--- src/pages/ItemsNewPage/Tags.tsx
+++ src/pages/ItemsNewPage/Tags.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { Icon } from '../../components/Icon'
 
 type Props = {
@@ -10,8 +11,10 @@ export const Tags: React.FC<Props> = ({ kind }) => {
     <div >
       <ol grid grid-cols="[repeat(auto-fit,48px)]" justify-center gap-x-32px gap-y-16px py-16px px-8px>
         <li>
+          <Link to={`/tags/new?kind=${kind}`}>
           <span w-48px h-48px rounded='24px' bg="#EFEFEF" flex justify-center items-center text-24px text="#7bb0e2"> <Icon name="add" />
-          </span>
+            </span>
+            </Link>
         </li>
         {tags.map((tag, index) =>
           <li key={index} w-48px flex justify-center items-center flex-col gap-y-8px>
