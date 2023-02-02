799c66bb212986026c024faf52cdf417711dc8ba
DownGrowth <1209157041@qq.com>
2023-02-02 21:02:15 +0800

    消除key的警告

--- src/components/Input/EmojiInput.tsx
+++ src/components/Input/EmojiInput.tsx
@@ -24,7 +24,7 @@ export const EmojiInput: React.FC<Props> = (props) => {
             grid grid-cols="[repeat(auto-fit,34px)]" grid-rows="[repeat(auto-fit,34px)]"
             justify-center>
             {emoji.chars.map(char =>
-               <span b-1 b-transparent className={char === value ? s.selected : ''} rounded-4px onClick={() => value !== char && onChange?.(char)}>{char}</span>)}
+               <span key={char} b-1 b-transparent rounded-4px className={char === value ? s.selected : ''} onClick={() => value !== char && onChange?.(char)}>{char}</span>)}
           </div>)}
       </div>
     </div>
