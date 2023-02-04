fd4c2544edad2b48779fc108f4d9b3c886f3801a
DownGrowth <1209157041@qq.com>
2023-02-04 22:14:32 +0800

    消除警告

--- src/components/RankChart.tsx
+++ src/components/RankChart.tsx
@@ -12,7 +12,7 @@ export const RankChart: React.FC<Props> = (props) => {
   const renderItems = () => {
     return (
       items?.map((item, index) =>
-        <div grid grid-cols="[48px_1fr_1fr]" grid-rows="[repeat(2,1fr)]"
+        <div key={item.name} grid grid-cols="[48px_1fr_1fr]" grid-rows="[repeat(2,1fr)]"
           text-12px items-center gap-y-6px gap-x-8px px-16px my-8px>
           <div row-start-1 col-start-1 row-end-3 col-end-2
             w-48px h-48px rounded-24px bg="#EFEFEF" flex justify-center items-center
@@ -21,7 +21,7 @@ export const RankChart: React.FC<Props> = (props) => {
           <div row-start-1 col-start-3 row-end-2 col-end-4 text-right self-end>
             <Money value={item.value}/>
           </div>
-          <div row-start-2 col-start-2 row-end-3 col-end-4 h-8px self-startrounded-4px overflow-hidden bg="#CCC" relative>
+          <div row-start-2 col-start-2 row-end-3 col-end-4 h-8px self-start rounded-4px overflow-hidden bg="#CCC" relative>
             <div absolute h-full rounded-4px
               style={{ background: colors[index], width: `${item.value / max * 100}%` }} />
             </div>
