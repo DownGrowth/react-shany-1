836645bee69aeae4f75d000e99cb7071975884ce
DownGrowth <1209157041@.com>
2023-02-25 20:22:59 +0800

    Ajax 统一错误处理

--- src/lib/ajax.ts
+++ src/lib/ajax.ts
@@ -1,5 +1,6 @@
-import type { AxiosRequestConfig } from 'axios'
+import type { AxiosError, AxiosRequestConfig } from 'axios'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 import { useLoadingStore } from '../stores/useLoadingStore'
 // 静态配置项用 defaults 配置
 axios.defaults.baseURL = isDev ? '/' : 'http://121.196.236.94:8080/api/v1'
@@ -15,17 +16,43 @@ axios.interceptors.request.use((config) => {
 // 封装 axios
 type Options = {
   showLoading?: boolean
+  handleError?: boolean
 }
 export const useAjax = (options?: Options) => {
+  const table: Record<string, () => void> = {
+    401: () => {
+      nav('/sign_in')
+    },
+    402: () => {
+      window.alert('请付费后观看')
+    },
+    403: () => {
+      window.alert('没有权限')
+    },
+    unknown: () => {
+      window.alert('未知错误')
+    }
+  }
   const showLoading = options?.showLoading || false
+  const handleError = options?.handleError ?? true
   const { setVisible } = useLoadingStore()
+  const nav = useNavigate()
+  const onError = (error: AxiosError) => {
+    if (error.response) {
+      if (handleError) { const { status } = error.response
+        const fn = table[status] || table.unknown
+        fn?.()
+      }
+    }
+    throw error
+  }
   const ajax = {
     get: <T>(path: string, config?: AxiosRequestConfig<any>) => {
-      return axios.get<T>(path, config)
+      return axios.get<T>(path, config).catch(onError)
     },
     post: <T>(path: string, data: JSONValue) => {
       if (showLoading) { setVisible(true) }
-      return axios.post<T>(path, data).finally(() => { if (showLoading)
+      return axios.post<T>(path, data).catch(onError).finally(() => { if (showLoading)
       { setVisible(false) } })
     },
     patch: () => { },
