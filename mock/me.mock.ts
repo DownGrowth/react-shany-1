2168e61a39ea08cd65593be9ac85e03e01ab1270
DownGrowth <1209157041@.com>
2023-02-26 19:20:51 +0800

    把所有需要登录的页面放在一个路由组里进行拦截

--- mock/me.mock.ts
+++ mock/me.mock.ts
@@ -3,15 +3,15 @@ import type { MockMethod } from 'vite-plugin-mock'
 export const meMock: MockMethod = {
   url: '/api/v1/me',
   method: 'get',
-  statusCode: 401,
+  statusCode: 200,
   response: () => {
     return {
-      // resource: {
-      //   id: 1,
-      //   email: 'bk@bk.com',
-      //   updated_at: '2021-08-01T00:00:00.000Z',
-      //   created_at: '2021-08-01T00:00:00.000Z',
-      // }
+      resource: {
+        id: 1,
+        email: 'bk@bk.com',
+        updated_at: '2021-08-01T00:00:00.000Z',
+        created_at: '2021-08-01T00:00:00.000Z',
+      }
     }
   },
 }
