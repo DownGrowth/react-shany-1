a8adf1a82bf64ceabb3e8e2afe7532ac34359051
DownGrowth <1209157041@.com>
2023-03-02 22:41:03 +0800

    收集新建item表单的kind

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,9 +1,9 @@
 import type { ReactNode } from 'react'
-import { useState } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { Tabs } from '../components/Tabs'
 import { TopNav } from '../components/TopNav'
+import { useCreateItemStore } from '../stores/useCreateItemStore'
 import s from './ItemsNewPage.module.scss'
 import { DateAndAmount } from './ItemsNewPage/DateAndAmount'
 import { Tags } from './ItemsNewPage/Tags'
@@ -11,13 +11,16 @@ import { Tags } from './ItemsNewPage/Tags'
 export const ItemsNewPage: React.FC = () => {
   const tabItems: { key: Item['kind']; text: string; element?: ReactNode }[]
     = [{ key: 'expenses', text: '支出', element: <Tags kind="expenses"/> }, { key: 'income', text: '收入', element: <Tags kind="income" /> }]
-  const [tabItem, setTabItem] = useState<Item['kind']>('expenses')
+  const { data, error, setData, setError } = useCreateItemStore()
   return (
     <div className={s.wrapper} h-screen flex flex-col>
       <Gradient className="grow-0 shrink-0">
         <TopNav title='记一笔' icon={<Icon name="back" className='w-24px h-24px' />} />
         </Gradient>
-      <Tabs tabItems={tabItems} className="text-center grow-1 shrink-1 overflow-hidden" classPrefix='itemsNewPage' value={tabItem} onChange={tabItem => setTabItem(tabItem)} />
+      <Tabs tabItems={tabItems} className="text-center grow-1 shrink-1 overflow-hidden" classPrefix='itemsNewPage' value={data.kind!} onChange={kind => setData({ kind })} />
+      <div>
+       {JSON.stringify(data)}
+      </div>
       <DateAndAmount className="grow-0 shrink-0"/>
     </div>
   )
