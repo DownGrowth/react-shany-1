51bd2190492d92af144bbac4ec29c7877afad455
DownGrowth <1209157041@.com>
2023-03-15 20:57:22 +0800

    长按tag进入编辑页面

--- src/pages/ItemsNewPage/Tags.tsx
+++ src/pages/ItemsNewPage/Tags.tsx
@@ -1,4 +1,6 @@
-import { Link } from 'react-router-dom'
+import type { TouchEvent } from 'react'
+import { useRef } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import useSWRInfinite from 'swr/infinite'
 import { Icon } from '../../components/Icon'
@@ -35,6 +37,32 @@ export const Tags: React.FC<Props> = (props) => {
   const isLoadingMore = data?.[size - 1] === undefined && !error
   const isLoading = isLoadingInitialData || isLoadingMore
   const onLoadMore = () => { setSize(size + 1) }
+  const nav = useNavigate()
+  const touchTimer = useRef<number>()
+  const touchPosition = useRef<{ x?: number; y?: number }>({ x: undefined, y: undefined })
+  const onTouchStart = (e: TouchEvent<HTMLElement>, id: Tag['id']) => {
+    touchTimer.current = window.setTimeout(() => {
+      nav(`/tags/${id}`)
+    }, 800)
+    const { clientX: x, clientY: y } = e.touches[0]
+    touchPosition.current = { x, y }
+  }
+  const onTouchMove = (e: TouchEvent<HTMLElement>, id: Tag['id']) => {
+    const { clientX: newX, clientY: newY } = e.touches[0]
+    const { x, y } = touchPosition.current
+    if (x === undefined || y === undefined) { return }
+    const distance = Math.sqrt((newX - x) ** 2 + (newY - y) ** 2)
+    if (distance > 10) {
+      window.clearTimeout(touchTimer.current)
+      touchTimer.current = undefined
+    }
+  }
+  const onTouchEnd = (e: TouchEvent<HTMLElement>, id: Tag['id']) => {
+    if (touchTimer.current) {
+      window.clearTimeout(touchTimer.current)
+      touchTimer.current = undefined
+    }
+  }
   if (!data) {
     return <div>空</div>
   } else {
@@ -53,7 +81,10 @@ export const Tags: React.FC<Props> = (props) => {
               {data.map(({ resources }) => {
                 return resources.map((tag, index) =>
                   <li key={index} w-48px flex justify-center items-center flex-col gap-y-8px
-                  onClick={() => { onChange?.([tag.id]) }}
+                    onClick={() => { onChange?.([tag.id]) }}
+                    onTouchStart={e => onTouchStart(e, tag.id)}
+                    onTouchMove={e => onTouchMove(e, tag.id)}
+                    onTouchEnd={e => onTouchEnd(e, tag.id)}
                 >
                   {value?.includes(tag.id)
                     ? <span w-48px h-48px rounded='24px' bg="#EFEFEF" flex justify-center items-center text-24px b-1 b="#7bb0e2">{tag.sign}</span>
