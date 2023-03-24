b3ca195df1db4145776d5cb645ae2cefc39943ec
DownGrowth <1209157041@.com>
2023-03-24 19:48:44 +0800

    点击自定义时间会弹框

--- src/components/TimeRangePicker.tsx
+++ src/components/TimeRangePicker.tsx
@@ -1,3 +1,4 @@
+import { usePopup } from '../hooks/usePopup'
 import { Tabs } from './Tabs'
 export type TimeRange = 'thisMonth' | 'lastMonth' | 'thisYear' | 'custom' | 'twoMonthAgo' | 'threeMonthAgo'
 type Props = {
@@ -12,8 +13,26 @@ const defaultTimeRanges: { key: TimeRange; text: string }[] = [
   { key: 'custom', text: '自定义时间' },
 ]
 export const TimeRangePicker: React.FC<Props> = (props) => {
-  const { selected, onSelect, timeRanges = defaultTimeRanges } = props
+  const { selected, onSelect: _onSelect, timeRanges = defaultTimeRanges } = props
+  const onConfirm = () => {
+    _onSelect('custom')
+    hide()
+  }
+  const { popup, hide, show } = usePopup({
+    children: <div onClick={onConfirm}>弹框</div>,
+    position: 'center'
+  })
+  const onSelect = (key: TimeRange) => {
+    if (key === 'custom') {
+      show()
+    } else {
+      _onSelect(key)
+    }
+  }
   return (
+    <>
+    {popup}
     <Tabs tabItems={timeRanges} value={selected} onChange={onSelect } />
+    </>
   )
 }
