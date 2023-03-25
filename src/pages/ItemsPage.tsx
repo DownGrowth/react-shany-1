c47c47b8de154fc3dfd108843c2918cf4d60c60e
DownGrowth <1209157041@.com>
2023-03-25 21:19:09 +0800

    完成时间选择框，可以进行校验

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -6,17 +6,29 @@ import type { TimeRange } from '../components/TimeRangePicker'
 import { TimeRangePicker } from '../components/TimeRangePicker'
 import { TopMenu } from '../components/TopMenu'
 import { TopNav } from '../components/TopNav'
-import { time } from '../lib/time'
+import { Time, time } from '../lib/time'
 import { useMenuStore } from '../stores/useMenuStore'
 import { ItemsList } from './itemsPage/ItemsList'
 import { ItemSummary } from './itemsPage/ItemsSummary'
 
 export const ItemsPage: React.FC = () => {
-  const [timeRange, setTimeRange] = useState<TimeRange>({
+  const [timeRange, _setTimeRange] = useState<TimeRange>({
     name: 'thisMonth',
     start: time().firstDayOfMonth,
     end: time().lastDayOfMonth.add(1, 'day')
   })
+  const [outOfRange, setOutOfRange] = useState(false)
+  const setTimeRange = (t: TimeRange) => {
+    if (t.start.timestamp > t.end.timestamp) {
+      [t.start, t.end] = [t.end, t.start]
+    }
+    if (t.end.timestamp - t.start.timestamp > Time.DAY * 365) {
+      setOutOfRange(true)
+    } else {
+      setOutOfRange(false)
+    }
+    _setTimeRange(t)
+  }
   const { visible, setVisible } = useMenuStore()
   const { start, end } = timeRange
   return (
@@ -27,9 +39,13 @@ export const ItemsPage: React.FC = () => {
             onClick={() => { setVisible(!visible) }} />} />
            </Gradient>
         <TimeRangePicker selected={timeRange}
-          onSelect={ setTimeRange} />
-      <ItemSummary />
-      <ItemsList start={start} end={end} />
+        onSelect={setTimeRange} />
+      {outOfRange
+        ? <div text-center p-32px> 时间选择范围不能超过 365 天</div>
+        : <>
+        <ItemSummary />
+        <ItemsList start={start} end={end} /> </>
+      }
       <AddItemFloatButton />
       {<TopMenu visible={visible} onClickMask={() => { setVisible(false) }} />}
     </div>
