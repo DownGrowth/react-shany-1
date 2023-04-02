009829c252eda170c76692c983700a651f6c25d7
DownGrowth <1209157041@.com>
2023-04-02 22:53:55 +0800

    提供退出登录功能

--- src/pages/TagsEditPage.tsx
+++ src/pages/TagsEditPage.tsx
@@ -3,19 +3,14 @@ import { BackIcon } from '../components/BackIcon'
 import { Gradient } from '../components/Gradient'
 import { TopNav } from '../components/TopNav'
 import { useAjax } from '../lib/ajax'
+import { comfirmable } from '../lib/comfirmable'
 import { TagForm } from './TagsNewPage/TagForm'
 
 export const TagsEditPage: React.FC = () => {
-  const comfirmable = (fn: () => void) => () => {
-    const result = window.confirm('确定要删除吗？')
-    if (result) {
-      fn()
-    }
-  }
   const { id } = useParams()
   const nav = useNavigate()
   const { destroy } = useAjax({ showLoading: true, handleError: true })
-  const onDelete = comfirmable(async () => {
+  const onDelete = comfirmable('确定要删除吗？', async () => {
     if (!id) { throw new Error('id 不能为空') }
     await destroy(`/api/v1/tags/${id}`).catch((error) =>
     { window.alert('删除失败'); throw error }
